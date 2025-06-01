185d3206d09c92d89b0225e0d71c0a7f130dd73b
Balmukund Kumar <balmukund8294@gmail.com>
2025-06-01 19:14:25 +0530

    working

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,144 @@
+ 
+ 
+ //// Hello BUDDY , Starting Of JavaScript ............
+ 
+ 
+ 
+ 
+ /*let js = 'amazing';
+  
+  console. log(40+8+23-10);
+  
+  console. log("mukund");
+  console.log('23');
+
+  let firstName = "Kumar";  
+  
+  // variable name write using camel case notation   
+  
+  console.log(firstName);
+  console.log(firstName);
+  console.log(firstName);
+
+let Jonas_matilda ="BMK";
+let $function = 27;
+
+let person = "mukund";
+let PI = 3.1415;
+
+let myFirstJob = 'coder';
+let myCurrentJob = 'Teacher';
+
+letjob1 = "coder";
+letjob2 = 'Teacher';
+
+console.log(myFirstJob);  */
+
+
+
+
+
+
+
+
+/*let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'mukund');
+
+
+javascriptIsFun ='YES!';
+console.log(typeof javascriptIsFun);
+
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null); */
+
+
+
+
+
+
+
+/* let age = 30;
+age = 31;
+
+const birthYear = 2006;
+// birthYear = 1990;  (Wrong)
+
+var job = 'programmer';
+job = 'teacher';
+
+lastName = 'Kumar';
+console.log(lastName);  */
+
+
+
+
+
+/* const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas,ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the powe of 3 = 2 * 2 * 2
+
+const firstName = 'mukund';
+const lastName = 'satish';
+console.log(firstName + lastName);
+console.log(firstName + ' ' + lastName);
+
+// Assignment operators
+ let x = 10 + 5;  //15
+ x += 10; // x = x  + 10 = 25
+ x += 10; // x = x * 4 = 100
+ x--;
+ x--;
+
+ console.log(x);
+
+//Comparison operators
+console.log(ageJonas > ageSarah);  //  > , <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now -2018); */
+//  first (-) operator work then (> ) bcz precedence series
+  
+
+
+
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas,ageSarah);
+
+
+console.log(25 - 10 - 5 );   // 10 bcz  left to right 
+
+let x, y;
+x = y = 25 - 10 -5 ; // x = 10, x= 10   bcz right to left 
+console.log(x,y);
+
+
+const averageAge = ageJonas + ageSarah/2;
+console.log (ageJonas, ageSarah);
+
+
+
+
+
+
