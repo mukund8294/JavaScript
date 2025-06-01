d49c3e0509fff1ea48c9f35006d8aba1349adb3a
Balmukund Kumar <balmukund8294@gmail.com>
2025-06-01 23:58:16 +0530

    continue

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -283,3 +283,35 @@ console.log('23' /'2');
 let n = '1' + 1;    // '11'
 n= n -1;
 console.log(n);  */ 
+
+
+
+
+
+
+
+//5 falsy values: 0,'',undefined,null,NaN
+
+
+console.log(Boolean (0));
+console.log(boolean(undefined));
+console.log(Boolean('mukund'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+const  money = 100;
+if (money) {
+  console.log("Don't spend it all ;)");
+} else {
+console.log('You should get a job!');
+}
+
+
+let height = 100;
+if(height) {
+  console.log('YAY! Height is defined');
+} else {
+  console.log('Height is defined');
+}
+
+
