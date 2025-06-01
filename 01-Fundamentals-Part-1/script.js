7dd46009efa98b86dbc9230462d00c67e4a1071e
Balmukund Kumar <balmukund8294@gmail.com>
2025-06-02 01:26:50 +0530

    continue

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -223,6 +223,9 @@ console.log(century);   */
 
 
 
+
+//  Coding Challenge #3
+
 /* const massMark = 67;
  const heightMark = 1.73;
  const massJohn = 72;
@@ -396,4 +399,18 @@ if (hasDriversLicense && hasGoodVision && isTired) {
 
 
 
-  
+// Coding Challenge # 3
+const scoreDolphins = (99+103+80)/3;
+const scoreKoalas = (109+95+50)/3;
+console.log(scoreDolphins, scoreKoalas);
+
+if(scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+    console.log('Dolphins win the trophy');
+
+} else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
+    console.log('Both win the trophy');
+} else if (scoreDolphins == scoreKoalas && scoreDolphins >= 100 && scoreKoalas >= 100);
+else {
+    console.log('No one win the trophy');
+}
+    */
