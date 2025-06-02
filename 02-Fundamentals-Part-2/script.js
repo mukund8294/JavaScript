698966b5ec24f6cf80d0d45c40d8c21249e00af3
Balmukund Kumar <balmukund8294@gmail.com>
2025-06-02 05:49:32 +0530

    update

---
+++ 02-Fundamentals-Part-2/script.js
@@ -0,0 +1,15 @@
+/// Hello, Buddy......
+
+'use strict';
+/* 
+let hasDriversLicense = false;
+constpassTest = true;
+
+if (passTest) hasDriversLiciense = true;
+if(hasDriversLicense) console.log('I can drive :D');
+
+///const interface = 'Audio';
+// const private = 534:
+
+*/
+
