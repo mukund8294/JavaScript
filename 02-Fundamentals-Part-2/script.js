03951348a934685408945835c25b86f7520276e5
Balmukund Kumar <balmukund8294@gmail.com>
2025-06-02 06:04:51 +0530

    continue

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -13,3 +13,31 @@ if(hasDriversLicense) console.log('I can drive :D');
 
 */
 
+
+
+
+
+//////////////  Functions
+
+/*
+function logger(){
+    console.log('My name is Mukund');
+}
+
+//calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges}'oranges.`;
+    return juice;
+}
+
+const applejuice = fruitProcessor(5,0);
+console.Log(applejuice.juice);
+
+const appleOrange = fruitProcessor(10,5);
+console.Log(appleOrange.juice);
+*/
