55ce59ccffec8e27897cc9d61bad0df66faeca42
Balmukund Kumar <balmukund8294@gmail.com>
2025-06-03 10:50:10 +0530

    continue

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -2,8 +2,33 @@
 'use strict';
 
 /////////  Setup Prettier/////
+/*
 const x = '23';
 if (x === 23) console.log(23);
 const calcAge = birthYear => 2025 - birthYear;
 
 console.log(calcAge(2005));
+*/
+
+///// Problem - #1
+/*
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitude = calcTempAmplitude(temperatures);
+console.log(amplitude);
+*/
