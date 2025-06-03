16bd5bbfd3636c0d54b9d4d1a1476fe25596578c
Balmukund Kumar <balmukund8294@gmail.com>
2025-06-03 07:35:00 +0530

    continue

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -435,3 +435,22 @@ if (mark.bmi > john.bmi) {
 
 
 
+//////Iteration the for loop
+
+// console.log('Lifting weights repetition 1');
+// console.log('Lifting weights repetition 2');
+// console.log('Lifting weights repetition 3');
+// console.log('Lifting weights repetition 4');
+// console.log('Lifting weights repetition 5');
+// console.log('Lifting weights repetition 6');
+// console.log('Lifting weights repetition 7');
+// console.log('Lifting weights repetition 8');
+// console.log('Lifting weights repetition 9');
+// console.log('Lifting weights repetition 10');
+
+
+// for loop keeps running while condition is true
+
+for(let rep = 1; rep <=30; rep++) {
+    console.log(`Lifting wights repetition {rep}`);
+}
