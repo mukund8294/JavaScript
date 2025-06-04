838fcf4b35a74339a4c64a5bc01b6ced46fa3a36
Balmukund Kumar <balmukund8294@gmail.com>
2025-06-04 13:59:49 +0530

    continue

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -61,3 +61,86 @@ console.log(p, q, r);
 ///
 /////
 ///////
+// Destructuring Objeects
+/*
+const restaurant = {
+  Name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, ///////   Open 24 hours.....
+      close: 24,
+    },
+  },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+};
+
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+restaurant.order({
+  address: 'Via del Sole, 21',
+  starterIndex: 1,
+});
+
+const { Name, openingHours, categories } = restaurant;
+console.log(Name, openingHours, categories);
+
+const {
+  name: restaurantName,
+  oprningHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+// Default values...
+const { menu = [], starerMenu = [] } = restaurant;
+console.log(menu, starters);
+
+//mutating variables.......
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+({ a, b } = obj);
+console.log(a, b);
+
+//// Nested objects...........
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
+*/
+
+//
+////
+///////
