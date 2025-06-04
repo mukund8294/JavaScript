ebd7c64b1fe633aef1dddb3128fc7dce9dc09ff3
Balmukund Kumar <balmukund8294@gmail.com>
2025-06-05 00:40:13 +0530

    conitnue

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -1,1 +1,10 @@
 'use strict';
+////  Hello , buddy..........
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.show-modal');
+console.log(btnCloseModal);
+
+for (let i = 0; i < btnCloseModal.length; i++)
+  console.log(btnCloseModal[i].textContent);
