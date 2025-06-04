486d43b63fb6e2754d0a989ba61c147d90bfe875
Balmukund Kumar <balmukund8294@gmail.com>
2025-06-04 07:05:16 +0530

    continue

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -43,3 +43,54 @@ calcAge(1993);
 // console.log(age);
 //  printAge();
 */
+
+//
+//
+//
+////// Hoisting and TDZ in Prac.
+/*
+// Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'raj';
+let profession = 'teacher';
+const year = 2005;
+
+// Functions
+console.log(addDecl(2, 3));
+
+// console.log(addExpr(2, 3));
+
+//console.log(addArrow(2, 3));
+
+//.
+
+//.
+function addDecl(a, b) {
+  return a + b;
+}
+const addExpr = function (a, b) {
+  return a + b;
+};
+var addArrow = (a, b) => a + b;
+
+// Example
+console.log(undefined);
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
+*/
