4dec479b5125c8a2ee9e4298c317ef8627943966
Balmukund Kumar <balmukund8294@gmail.com>
2025-06-04 21:47:19 +0530

    continue

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -508,7 +508,7 @@ team1 > team2 && console.log('Team 2 is more likely to win');
 ////////////////////////////////////////////////////
 
 ////Looping arrays: the for-of loop.......
-
+/*
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
 for (const item of menu) console.log(item);
@@ -518,8 +518,60 @@ for (const [i, el] of menu.entries()) {
 }
 
 // console.log([...menu.entries()]);
-
+*/
 ////
 //
 /////////////////////////////////////////////////////
-//
+
+//////Enhanced Object Literals
+/*
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, ///////   Open 24 hours.....
+    close: 12 + 12,
+  },
+};
+
+const restaurant = {
+  Name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  /// ES6 enhanced object literals
+  openingHours,
+
+  order:(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery:({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+  orderPasta:(ing1, ing2, ing3) {
+    console.log(
+      `here is your declicious pasta with $ {ing1}, ${ing2} and ${ing3}`
+    );
+  },
+  orderPizza:(mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient), console.log(otherIngredients);
+  },
+};
+*/
