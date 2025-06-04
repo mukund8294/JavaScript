0f84544a8f19bc99ffc85c840266bb5cd6641457
Balmukund Kumar <balmukund8294@gmail.com>
2025-06-04 09:12:00 +0530

    continue

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -181,3 +181,64 @@ var addArrow = (a, b) => {
 };
 addArrow(2, 5, 8);
 */
+
+////// Primitive  Vs. Reference Values
+/*
+// primitive value
+
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age);  //31
+console.log(age); //30
+
+
+//// Reference value
+const me = {
+    name: 'mukund'
+    age: 30,
+};
+const friend = me;
+friend.age =27;
+
+console.log(`friend:`, friend);
+//{ name: 'mukund' , age : 27}
+
+console.log('Me:', me);
+*/
+
+// 2nd example
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
+console.log(lastName, oldLastName);
+
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+};
+
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Davis';
+console.log('Before marriage:', jessica);
+console.log('After marriage:', marriedJessica);
+
+// marriedJessica = {};
+
+// Copying objects
+const jessica2 = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+  family: ['Alice', 'Bob'],
+};
+
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Davis';
+
+jessicaCopy.family.push('Mary');
+jessicaCopy.family.push('John');
+
+console.log('Before marriage:', jessica2);
+console.log('After marriage:', jessicaCopy);
