61e5c6971f09046035d68230aebbc615e76beee7
Balmukund Kumar <balmukund8294@gmail.com>
2025-06-05 19:31:57 +0530

    continue

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -866,3 +866,74 @@ console.log([...question.values()]);
 ///////////////////////////////////////
 
 ////// Coding Challenge #3
+//
+/*
+const gameEvents = new Map([
+  [17, 'GOAL'],
+  [36, 'Substitution'],
+  [47, 'GOAL'],
+  [61, 'Substitution'],
+  [64, 'Yellow card'],
+  [69, 'Red card'],
+  [70, 'Substitution'],
+  [72, 'Substitution'],
+  [76, 'GOAL'],
+  [80, 'GOAL'],
+  [92, 'Yellow card'],
+]);
+
+// 1.
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+// /2.
+gameEvents.delete(64);
+
+//// 3.
+console.log(`An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+const time = [...gameEvents.keys()].pop();
+console.log(time);
+console.log(` An event happened, on average, every ${time / gameEvents.size} minutes`
+);
+
+/// 4.
+for (const [min, event] of gameEvents) {
+  const half = min <= 45 ? 'FIRST' : 'SECOND';
+  console.log(`[${half} HALF] ${min}: ${event}`);
+}
+  */
+
+///
+//////
+///////////////////////////////////////////
+
+/////
+/////lecture : wORKING___wITH __Strings  part #1
+
+const airline = 'AIR Indira';
+const plane = 'A320';
+
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[3]);
+console.log(B737[0]);
+
+console.log(airline.length);
+console.log('B737.length');
+
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('r'));
+console.log(airline.lastIndexOf('Portugal'));
+
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7));
+
+console.log(airline.slice(0, airline.indexOf('
+  ')));
+  console.log(airline.slice(airline.lastIndexOf(' ') + 1 ));
+
+  console.log(airline.slice(-2));
+  console.log(airline.slice(1, -1));
+  
+
