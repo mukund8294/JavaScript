509780d6e99a30eafff6f19668936c26538e269e
Balmukund Kumar <balmukund8294@gmail.com>
2025-06-05 21:05:03 +0530

    continue

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -949,8 +949,132 @@ checkMiddleSeat('11B');
 checkMiddleSeat('23C');
 checkMiddleSeat('3E');
 
-console.log(new String('jonas'));
-console.log(typeof new String('jonas'));
+console.log(new String('mukund'));
+console.log(typeof new String('mukund'));
 
-console.log(typeof new String('jonas').slice(1));
+console.log(typeof new String('mukund').slice(1));
+*/
+
+///
+/////
+///////////////////////////////////////////
+
+//// lecture : wORKING___wITH __Strings  part #2
+/*
+const airline = 'TAP Air Portugal';
+
+console.log(airline.toLowerCase());
+console.log(airline.toUpperCase());
+
+//// fix capitalization in name
+const passenger = 'mUkUnD'; //////Mukund
+const passengerLower = passenger.toLowerCase();
+const passengerCorrect =
+  passengerLower[0].toUpperCase() + passengerLower.slice(1);
+console.log(passengerCorrect);
+
+//// Comparing emails...
+const email = 'balmukund@.io';
+const loginEmail = '  Balmukund@.Io \n';
+
+////const lowerEmail = loginEmail.toLowerCase();
+////const trimmedEmail = lowerEmail();
+const normalizedEmail = loginEmail.toLowerCase().trim();
+console.log(normalizedEmail);
+console.log((email = normalizedEmail));
+
+///// replacing...
+const priceGB = '216,17E';
+const priceUS = priceGB.replace('E', '$').replace(',', '.');
+console.log(priceUS);
+
+const announcement =
+  'All passengers come to boarding door 23. Boarding door 23!';
+
+console.log(announcement.replace('door', 'gate'));
+//console.log(announcement.replaceAll('door', 'gate'));
+
+console.log(announcement.replace(/door/g, 'gate'));
+
+//// Booleans......
+const plane = 'Airbus A320neo';
+console.log(plane.includes('A320'));
+console.log(plane.includes('Boeing'));
+console.log(plane.startsWith('Airbus'));
+
+if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
+  console.log('Part of the NEW ARirbus family');
+}
+
+//// Practice exercise......
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase();
+
+  if (baggage.includes('knife') || baggage.includes('gun')) {
+    console.log('You are NOT allowed on board');
+  } else {
+    console.log('Welcome aboard!');
+  }
+};
+
+checkBaggage('I have a laptop, some Food and a pocket Knife');
+checkBaggage('Socks and camera');
+checkBaggage('Got some snacks and a gun for protection');
+*/
+
+//
+/////
+//////////////////////////////////////////
+
+/// lecture : wORKING___wITH __Strings  part #3
+
+/*
+//// Split and joins...
+console.log('a+very+nice+string'.split('+'));
+console.log('Mukund Kumar '.split(' '));
+
+const [firstName, lastName] = 'Mukund Kumar'.split(' ');
+
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(' ');
+  const namesUpper = [];
+
+  for (const n of names) {
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+  console.log(namesUpper.join(' '));
+};
+
+capitalizeName('jessica ann smith davis');
+capitalizeName('Mukund Kumar ');
+
+//// Padding.......
+const message = 'Go to gate 23!';
+console.log(message.padStart(20, '+').padEnd(30, '+'));
+console.log('Mukund'.padStart(20, '+').padEnd(30, '+'));
+
+const maskCreditCard = function (number) {
+  const str = number + '';
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*');
+};
+
+console.log(maskCreditCard(245899865));
+console.log(maskCreditCard(457688767555434));
+console.log(maskCreditCard('65567876895435799'));
+
+//// Repeat......
+const message2 = 'Bad waether... All Departues Delayed... ';
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line ${'planes picture'.repeat(n)}`);
+};
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
 */
