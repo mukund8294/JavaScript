2ac5fa1f4f46f1ffef13dcb4ed2b98439c76e10d
Balmukund Kumar <balmukund8294@gmail.com>
2025-06-05 12:39:33 +0530

    continue

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -663,3 +663,32 @@ else console.log('user array empty');
 //////////////////////////////////////////////
 
 /// hold // 14//
+
+//
+/////
+///////////////////
+
+///  add more codes in this area..
+
+//// Property Names
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `We are open on ${properties.length} days: `;
+for (const day of properties) {
+  openStr += `${day}, `;
+}
+console.log(openStr);
+
+//// Property Values
+const values = Object.values(openingHours);
+console.log(values);
+
+//// Entire object
+const entries = Object.entries(openingHours);
+// console.log(entries);
+
+//// [key, value]
+for (const [day, { open, close }] of entries) {
+  console.log(`On ${day} we open at ${open} and close at ${close}`);
+}
