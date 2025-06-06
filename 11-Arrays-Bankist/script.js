562d883618c6b53373b0340c679e827b6d3d070e
Balmukund Kumar <balmukund8294@gmail.com>
2025-06-07 03:53:42 +0530

    continue

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -398,3 +398,38 @@ console.log(avg1, avg2);
 /////////...................................................................///////
 
 /////.....016 The Magic of Chaining Methods...
+
+/*
+const eurToUsd = 1.1;
+console.log(movements);
+
+// Pipeline.....
+const totalDepositsUSD = movements
+  .filter(mov => mov > 0)
+  .map((mov, i, arr) => {
+    // console.log(arr);
+    return mov * eurToUsd;
+  })
+  // .map(mov => mov * eurToUsd)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(totalDepositsUSD);
+*/
+
+///////............................................................///////
+
+////......017 Coding Challenge #3....
+//
+
+/*
+const calcAverageHumanAge = ages =>
+  ages
+    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+/// adults.length
+
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+console.log(avg1, avg2);
+
+*/
