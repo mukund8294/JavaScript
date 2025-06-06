9b0cdf3848bd8dd5e502600ea88e149dd02743cf
Balmukund Kumar <balmukund8294@gmail.com>
2025-06-06 08:59:37 +0530

    continue

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -324,3 +324,19 @@ console.log(notPrivate);
 ////////////////////////////////////////////////
 
 ///Closures
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+
+const booker = secureBooking();
+
+booker();
+booker();
+booker();
+
+console.dir(booker);
