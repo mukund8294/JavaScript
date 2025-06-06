54ceb7aed717aa283421753fd06a6343bbf3da51
Balmukund Kumar <balmukund8294@gmail.com>
2025-06-07 02:36:31 +0530

    continue

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -255,3 +255,63 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 //
 ////......009 Coding Challenge #1........
+
+/*
+const checkDogs = function (dogsJulia, dogsKate) {
+  const dogsJuliaCorrected = dogsJulia.slice();
+  dogsJuliaCorrected.splice(0, 1);
+  dogsJuliaCorrected.splice(-2);
+  // dogsJulia.slice(1, 3);
+  const dogs = dogsJuliaCorrected.concat(dogsKate);
+  console.log(dogs);
+
+  dogs.forEach(function (dog, i) {
+    if (dog >= 3) {
+      console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old`);
+    } else {
+      console.log(`Dog number ${i + 1} is still a puppy `);
+    }
+  });
+};
+
+// checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+*/
+
+///////.................................................................//////
+
+///
+///// Data Transformations_ map, filter, reduce........
+
+//////................................................................./////
+
+////
+////.... 011 The map Method.mp4......
+
+/*
+ const movementsUSD = movements.map(function (mov) {
+   return mov * eurToUsd;
+ });
+*/
+/*
+const movementsUSD = movements.map(mov => mov * eurToUsd);
+
+console.log(movements);
+console.log(movementsUSD);
+
+const movementsUSDfor = [];
+for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
+console.log(movementsUSDfor);
+
+const movementsDescriptions = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+      mov
+    )}`
+);
+console.log(movementsDescriptions);
+*/
+
+/////................................................................//////
+
+////...12 Computing Usernames.mp4.....
