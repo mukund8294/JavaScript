667d86050bf3f486bf198cd77da4647c8c9c2b1d
Balmukund Kumar <balmukund8294@gmail.com>
2025-06-07 01:13:53 +0530

    continue

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -110,3 +110,19 @@ console.log([...arr, ...arr2]);
 /////////// Join.....
 console.log([letters.join(' - ')]);
 */
+
+//////......................................................................///////////////
+
+///// The new at Method..
+
+const arr = [23, 11, 64];
+console.log(arr[0]); //// 23
+console.log(arr.at(0)); //// 23
+
+//// getting last array element.......
+console.log(arr[arr.length - 1]); /////64
+console.log(arr.slice(-1)[0]); /////64
+console.log(arr.at(-1)); //////64
+
+console.log('farhan'.at(0));
+console.log('farhan'.at(-1));
