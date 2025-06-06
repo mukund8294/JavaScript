a49e8682045b094f4ef250ec97825221aa3e5e03
Balmukund Kumar <balmukund8294@gmail.com>
2025-06-06 23:30:29 +0530

    conitnue

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -469,3 +469,27 @@ console.log(Date.now());
 future.setFullYear(2040);
 console.log(future);
 */
+
+///////////////////////*********************************************************///////////// */
+
+//////  Adding Dates to _Bankist_ App
+
+/////adding data in above ConvolverNode...account1.
+
+////////////***************************************************************** */ */
+
+////// Operations With Dates.,,,,,,,,,
+/*
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(+future);
+
+const calcDaysPassed = (date1, date2) =>
+  Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
+
+const days1 = calcDaysPassed(new Date(2037, 3, 4), new Date(2037, 3, 14));
+console.log(days1);
+
+/// add  more code in above
+*/
+
+//////////*******************************************************//////////////////
