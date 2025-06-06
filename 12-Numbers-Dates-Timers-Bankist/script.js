5aa66e83f451c2e44a49f31c4a819b15923d2406
Balmukund Kumar <balmukund8294@gmail.com>
2025-06-06 22:13:46 +0530

    continue

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -372,4 +372,22 @@ labelBalance.addEventListener('click', function () {
 
 /////.....................................................................................////////
 
-/////
+///// Numeric Separators.m.........
+
+/*
+//// 287,460,000,000
+const diameter = 287_460_000_000;
+console.log(diameter);
+
+const pricecents = 345_99;
+console.log(pricecents);
+
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+
+const PI = 3.1415;
+console.log(PI);
+
+console.log(Number('230_000'));
+console.log(parseInt('230_000'));
+*/
