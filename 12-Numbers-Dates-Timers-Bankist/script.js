e2ab7b52d542ecb6746745f81233d3cd58ab1a12
Balmukund Kumar <balmukund8294@gmail.com>
2025-06-06 23:37:28 +0530

    continue

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -521,5 +521,22 @@ console.log(
 //////  Timers_ setTimeout and setInterval.......
 
 //// adding more code in above codes,,,
-
+/*
 ////////setTimeout,,,,,,,,,
+const ingredients = ['olives', 'spinach'];
+const pizzaTimer = setTimeout(
+  (ing1, ing2) => console.log(`Here is your pizza with ${ing1} and ${ing2}`),
+  3000,
+  ...ingredients
+);
+console.log('Waiting...');
+
+if (ingredients.includes('spinach')) clearTimeout(pizzaTimer);
+
+///// setInterval......
+
+setInterval(function () {
+  const now = new Date();
+  console.log(now);
+}, 1000);
+*/
