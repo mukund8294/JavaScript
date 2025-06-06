05777b5c1d23026233bb59d304afe7f9562b6d9f
Balmukund Kumar <balmukund8294@gmail.com>
2025-06-06 05:58:21 +0530

    conitnue

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -78,6 +78,30 @@ checkIn(flight, mukund);
 //
 ///
 ///Functions Accepting Callback Functions.
+
 const oneWord = function (str) {
-  return;
+  return str.replace(/ /g, ' ').toLowerCase();
 };
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+//////Higher-order function...................
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+//// JS callbacks all the time.....
+const high5 = function () {
+  console.log('👋');
+};
+document.body.addEventListener('click', high5);
+['mukund', 'raj', 'sourav'].forEach(high5);
