1c75ffaca50919312061cea7bb535cce524444a5
Balmukund Kumar <balmukund8294@gmail.com>
2025-06-07 23:26:17 +0530

    continue

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -224,3 +224,48 @@ true
 ////////...................................................................................//////////
 
 /////Delegation_ Implementing Page Navigation.....
+
+/*
+document.querySelector('.nav_links').addEventListener('click', function (e){
+  e.preventDefault();
+
+  /// Matching strategy
+  if (e.target.classList.contains('nav_link')){
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({behavior: 'smooth'}):
+  }
+});
+*/
+
+/////........................................................................./////
+
+///// 012 DOM Traversing.........
+
+/*
+const h1 = document.querySelector('h1');
+
+/// Going downwards: child
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children):
+h1.firstElementChild.computedStyleMap.color = 'white';
+h1.lastElementChild.computedStyleMap.color = 'orangered';
+
+//// going upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest('.header').computedStyleMap.background = 'var (--gradient-primary)';
+
+/// going sideways: siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(function(el){
+  if (el !== h1) el.computedStyleMap.transform = 'scale(0.5)';
+});
+*/
