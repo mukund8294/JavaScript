1d958290acc15b1b87b0a02de014d233c0c56bdc
Balmukund Kumar <balmukund8294@gmail.com>
2025-06-08 00:24:39 +0530

    working

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -341,3 +341,33 @@ window.addEventListener('scroll', function () {
   else this.navigator.classList.add('sticky');
 });
 */
+
+/////............................................................................................./////
+
+//// 016 A Better Way_ The Intersection Observer API.....
+
+/*
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+console.log(navHeight);
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  // console.log(entry);
+
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: -${navHeight}px,
+});
+
+headerObserver.observe(header);
+*/
+
+/////............................................................................................////
+
+/////
