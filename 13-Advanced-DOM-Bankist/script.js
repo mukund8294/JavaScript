81f64322de2a541bc5b2bcf545235bc8e36dff72
Balmukund Kumar <balmukund8294@gmail.com>
2025-06-07 19:19:24 +0530

    continue

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -33,3 +33,22 @@ document.addEventListener('keydown', function (e) {
 ////////............................................................................................./////
 
 //////////.....HELLO, BUDDY...........///////
+
+/////.............................................................................................////
+
+///Selecting elements...
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByClassName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+////
