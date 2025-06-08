566baabdfe44db08a20e6b70e1d0f174553d27b2
Balmukund Kumar <balmukund8294@gmail.com>
2025-06-08 23:54:34 +0530

    continue

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -67,6 +67,7 @@ console.log(mukund.hasOwnProperty('species'));
 
 /*
 console.log(mukund.__proto__);
+
 ///Object.prototype (top of prototype chain)
 console.log(mukund.__proto__.__proto__);
 console.log(mukund.__proto__.__proto__.__proto__);
@@ -90,3 +91,32 @@ console.dir(x => x + 1);
 */
 
 ////...............................................................................................////
+
+////// Coding Challenge #1
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+const bmw = new Car('BMW', 120);
+const mercedes = new Car('Mercedes', 95);
+
+bmw.accelerate();
+bmw.accelerate();
+bmw.brake();
+bmw.accelerate();
+
+////..............................................................................................////////
+
+//// ES6 Classes....
