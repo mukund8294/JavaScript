49e0ca0dcfa3f436d29d6066d50cc9f2fba23062
Balmukund Kumar <balmukund8294@gmail.com>
2025-06-09 00:32:04 +0530

    working

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -120,3 +120,88 @@ bmw.accelerate();
 ////..............................................................................................////////
 
 //// ES6 Classes....
+
+/*
+/// Class expression...
+// const PersonCl = class {}
+
+/// Class declaration...
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  // Methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.fullName}`);
+  }
+
+  get age() {
+    return 2025 - this.birthYear;
+  }
+
+  /// Set a property that already exists...
+  set fullName(name) {
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+
+  /// Static method....
+  static hey() {
+    console.log('Hey there 👋👋👋👋👋');
+    console.log(this);
+  }
+}
+
+const jessica = new PersonCl('Jessica', prototype);
+console.log(jessica);
+jessica.calcAge();
+
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+// PersonCl.prototype.greet = function () {
+// console.log(`Hey ${this.firstName}`);
+// };
+jessica.greet();
+
+///1. Classes are NOT hoisted
+/// 2. Classes are first- class citizes
+/// 3. Classes are executed in strict mode
+
+const walter = new PersonCl('Walter White', 1965);
+*/
+
+////............................................................................................./////
+
+//// Setters & Getters......
+
+/*
+const account = {
+  owner: 'RAM...',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements);
+*/
+
+////................................................................................................/////
