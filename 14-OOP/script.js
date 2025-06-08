b83986959e770e390d71c6b2cc15478a44bf2f62
Balmukund Kumar <balmukund8294@gmail.com>
2025-06-08 22:43:03 +0530

    start

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+/////...............................................................................................//////
+
+///// Constructor Functions and the new Operator.....
+
+const person = function (firstName, birthYear) {
+  /// Instance properties....
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  /// Never to this
+  /// this.calcAge = function {
+  /// console.log(2037 - this.birthYear);
+  /// };
+};
+
+const mukund = new Person('Mukund', 2005);
+console.log(jonas);
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+console.log(matilda, jack);
+
+console.log(jonas instanceof Person);
