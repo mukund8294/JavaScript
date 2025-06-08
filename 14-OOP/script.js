8dd1d4795cac00c1cb5c2b96084c65ff5f759260
Balmukund Kumar <balmukund8294@gmail.com>
2025-06-09 02:13:40 +0530

    working

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -425,3 +425,33 @@ martha.calcAge();
 //// ............................................................................................../////
 
 //// Inheritance Between _Classes__ Object.create....
+
+/*
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+
+const StudentProto = Object.create(PersonProto);
+StudentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+StudentProto.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const jay = Object.create(StudentProto);
+jay.init('Jay', 2010, 'Computer Science');
+jay.introduce();
+jay.calcAge();
+*/
