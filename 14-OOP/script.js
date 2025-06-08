097329d52612b05473d7bb15997e1fbd29a8704c
Balmukund Kumar <balmukund8294@gmail.com>
2025-06-09 01:24:36 +0530

    continue

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -272,3 +272,42 @@ ford.speedUS = 50;
 console.log(ford);
 
 ////................................................................................................/////
+
+//// Inheritance Between _Classes__ Constructor Functions.....
+
+/*
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+const Student = function (firstName, birthYear, course) {
+  Person.call(this, firstName, birthYear);
+};
+this.course = course;
+
+/// Linking prototypes...
+Student.prototype = Object.create(Person.prototype);
+
+Student.prototype.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const mike = new Student('mike', 2020, 'Computer Science');
+mike.introduce();
+mike.calcAge();
+
+console.log(mike._proto_);
+console.log(mike._proto.proto_);
+
+console.log(mike instanceof Student);
+console.log(mike instanceof Person);
+console.log(mike instanceof Object);
+
+Student.prototype.constructor = Student;
+console.dir(Student.prototype.constructor);
+**/
