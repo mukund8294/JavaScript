3c9e135b4d4534a531425159d07dc9aebc5cfc10
Balmukund Kumar <balmukund8294@gmail.com>
2025-06-09 19:25:19 +0530

    hold

--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -10,3 +10,7 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//////...........hold.....................///////////
