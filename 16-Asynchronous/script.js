cd489d931e33ac59080926ebddf7b67dac77f6c2
Balmukund Kumar <balmukund8294@gmail.com>
2025-06-13 04:01:13 +0530

    Completed

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const btn = document.querySelector('.btn-country');
-const countriesContainer = document.querySelector('.countries');
+///const btn = document.querySelector('.btn-country');
+///const countriesContainer = document.querySelector('.countries');
 
 // NEW COUNTRIES API URL (use instead of the URL shown in videos):
 // https://restcountries.com/v2/name/portugal
