7620d818988db12b91bd7585ccf233bed437efd2
Balmukund Kumar <balmukund8294@gmail.com>
2025-06-12 21:05:25 +0530

    continue

--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -63,17 +63,34 @@ if (navigator.geolocation)
       const { longitude } = position.coords;
       console.log(`https://www.google.pt/maps/@${laltiutde},${longitude}`);
 
+      const coords = [laltiutde, longitude];
+
       const map = L.map('map').setView(coords, 13);
+      /// console.log(map);
 
       L.tileLayer('https//{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href ="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributtors',
       }).addTo(map);
 
-      L.marker(coords)
+      map.on('click', function (mapEvent){
+        console.log(mapEvent);
+        const { lat, lng }  = mapEvent.latlng;
+
+      L.marker(lat, lng) 
         .addTo(map)
-        .bindPopup('A preetty CSS3 popup.<br> Easily customizable.')
+        .bindPopup(
+          L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false;
+            className: 'running-popup',
+      })
+        )
+        .setPopupContent('workout')
         .openPopup();
+        });
     },
     function () {
       alert('Could not get your position');
