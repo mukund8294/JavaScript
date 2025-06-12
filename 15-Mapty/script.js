c02cf847b1c1825b76f293458d4e051c688fd989
Balmukund Kumar <balmukund8294@gmail.com>
2025-06-12 20:27:52 +0530

    continue

--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -62,6 +62,18 @@ if (navigator.geolocation)
       const { laltiutde } = position.coords;
       const { longitude } = position.coords;
       console.log(`https://www.google.pt/maps/@${laltiutde},${longitude}`);
+
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https//{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href ="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributtors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A preetty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
     },
     function () {
       alert('Could not get your position');
