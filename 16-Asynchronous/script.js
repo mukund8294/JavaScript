c5779bde3ade9dc2ab505fc68f862e890836e9e3
Balmukund Kumar <balmukund8294@gmail.com>
2025-06-13 07:47:37 +0530

    continue

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -13,6 +13,33 @@
 
 ////////////////////... HELLO, BUDDY....../////////////////////////////////////////
 
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then(response => {
+    if (`!response.ok) throw new Error(${errorMsg} (${response.status})`);
+
+    return response.json();
+  });
+};
+
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
 //////................................................................................................////
 
 //// XMLHttpRequest..........
