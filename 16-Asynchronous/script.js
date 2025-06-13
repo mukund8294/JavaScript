6d87782ccf416d02b13cb66abf55dde102acb63a
Balmukund Kumar <balmukund8294@gmail.com>
2025-06-13 09:44:26 +0530

    continue

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -417,3 +417,56 @@ createImage('img/img-1.jpg')
 ///Consuming Promises with Async_Await.....
 and;
 ////Error Handling With try...catch.....
+
+getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+
+// fetch(https://restcountries.com/v2/name/${country}).then(res => console.log(res))
+
+whereAmI = async function () {
+  try {
+    // Geolocation
+    const pos = await getPosition();
+    const { latitude: lat, longitude: lng } = pos.coords;
+
+    // Reverse geocoding
+    const resGeo = await fetch(`
+      https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`);
+    if (!resGeo.ok) throw new Error('Problem getting location data');
+
+    const dataGeo = await resGeo.json();
+    console.log(dataGeo);
+
+    // Country data
+    const res = await fetch(`
+      https://restcountries.com/v2/name/${dataGeo.countryCode}`);
+
+    // BUG in video:
+    // if (!resGeo.ok) throw new Error('Problem getting country');
+
+    // FIX:
+    if (!res.ok) throw new Error('Problem getting country');
+
+    const data = await res.json();
+    console.log(data);
+    renderCountry(data[0]);
+  } catch (err) {
+    console.error(`${err} 💥`);
+    renderError(`💥 ${err.message}`);
+  }
+};
+whereAmI();
+whereAmI();
+whereAmI();
+console.log('FIRST');
+
+// try {
+//   let y = 1;
+//   const x = 2;
+//   y = 3;
+// } catch (err) {
+//   alert(err.message);
+// }
