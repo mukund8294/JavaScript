b8003c162f68d6d1361a17af6592ab079cd0e5c5
Balmukund Kumar <balmukund8294@gmail.com>
2025-06-13 08:10:46 +0530

    Continue

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -139,3 +139,32 @@ Consuming Promises
 Chaining Promises
 Handling Rejected Promises
 Throwing Errors Manually
+
+const getCountryData = function (country) {
+
+  getJSON(
+    https://restcountries.com/v2/name/${country},
+    'Country not found'
+  )
+    .then(data => renderCountry(data, 'neighbour'))
+    .catch(err => {
+      console.error(`${err} `);
+      renderError(Something went wrong ${err.message}. Try again!);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    });
+
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+
+      if (!neighbour) throw new Error('No neighbour found!');
+
+      // Country 2
+      return getJSON(
+        https://restcountries.eu/rest/v2/alpha/${neighbour},
+        'Country not found'
+      );
+    })
+};
