ca1ae21769f5784c08add52c94cef2910d5336fe
Balmukund Kumar <balmukund8294@gmail.com>
2025-06-13 07:56:55 +0530

    Continue

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -119,3 +119,18 @@ const getCountryAndNeighbour = function (country) {
     });
   });
 };
+
+getCountryAndNeighbour('usa');
+
+setTimeout(() => {
+  console.log('1 second passed');
+  setTimeout(() => {
+    console.log('2 seconds passed');
+    setTimeout(() => {
+      console.log('3 second passed');
+      setTimeout(() => {
+        console.log('4 second passed');
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
