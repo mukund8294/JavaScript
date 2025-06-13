d5a07e237bb82b3ac284b19f89efa24ba2bab43d
Balmukund Kumar <balmukund8294@gmail.com>
2025-06-13 10:06:10 +0530

    continue

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -530,3 +530,20 @@ console.log('1: Will get location');
   console.log('3: Finished getting location');
 })();
 */
+
+////.................................................................................................////
+
+//// Running Promises in Parallel......
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    const data = await Promise.all([
+      getJSON(`https://restcountries.com/v2/name/${c1}`),
+      getJSON(`https://restcountries.com/v2/name/${c2}`),
+      getJSON(`https://restcountries.com/v2/name/${c3}`),
+    ]);
+    console.log(data.map(d => d[0].capital));
+  } catch (err) {
+    console.error(err);
+  }
+};
+get3Countries('portugal', 'canada', 'tanzania');
